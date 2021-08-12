a74c523a7b6068ebabcb05cf7d777d40e9b12d9a
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-08-12 14:08:26 +0530

    Added the Triangle Quiz

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,8 +1,21 @@
 var quizBtn = document.querySelector(".quiz-btn");
 var quizResult = document.querySelector(".quiz-result");
+var form = document.querySelector(".form");
+
+var correctAnswers = ["45°", "right", "An angle that measures less than 90°", "5cm", "Isosceles", "30°", "35 sq cm", "Yes", "62.35", "3 cm"];
 
 function quizResultHandler() {
-
+    let score = 0, index = 0;
+    var formResult = new FormData(form);
+    for (let value of formResult.values()) {
+        if (value === correctAnswers[index]) {
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    quizResult.classList.remove("hide");
+    quizResult.classList.add("display");
+    quizResult.innerText = "Your score is " + score;
 }
 
-quizResult.addEventListener("click", quizResultHandler);
+quizBtn.addEventListener("click", quizResultHandler);
