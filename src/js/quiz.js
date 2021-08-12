3e98507b4e9a1f41cfba6ad1185282ab5a885114
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-08-12 22:14:19 +0530

    feat: Answer validation in triangle-Quiz

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,8 +1,40 @@
 var quizBtn = document.querySelector(".quiz-btn");
 var quizResult = document.querySelector(".quiz-result");
 var form = document.querySelector(".form");
+var quizBox = document.querySelectorAll(".quiz-container");
 
 var correctAnswers = ["45°", "right", "An angle that measures less than 90°", "5cm", "Isosceles", "30°", "35 sq cm", "Yes", "62.35", "3 cm"];
+
+
+function colorIndicator() {
+    var index = 0, countor = 0;
+    var formResult = new FormData(form);
+    for (let value of formResult.values()) {
+        if (value !== correctAnswers[index]) {
+            quizBox[index].classList.add("red-alert");
+            countor++;
+        }
+        index = index + 1;
+    }
+}
+
+
+function checkCounter() {
+    var index = 0, countor = 0;
+    var formResults = new FormData(form);
+
+    for (value of formResults.values()) {
+        if (correctAnswers[index] === value)
+            countor++;
+        index++;
+    }
+
+    var wrongAnswers = 10 - countor;
+    var rightAnswers = countor;
+
+    return quizResult.innerText = "Correct Answers: " + rightAnswers + "\n" + "Wrong Answers: " + wrongAnswers;
+}
+
 
 function quizResultHandler() {
     let score = 0, index = 0;
@@ -13,9 +45,13 @@ function quizResultHandler() {
         }
         index = index + 1;
     }
-    quizResult.classList.remove("hide");
-    quizResult.classList.add("display");
-    quizResult.innerText = "Your score is " + score;
+
+    colorIndicator();
+
+    // quizResult.classList.remove("hide");
+    // quizResult.classList.add("display");
+    quizResult.innerText = checkCounter() + "\n" +  "Your score is " + score; 
 }
+
 
 quizBtn.addEventListener("click", quizResultHandler);
